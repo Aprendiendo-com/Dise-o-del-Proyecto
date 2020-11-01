a0460a858b77c94df5986a8cb3e89b8829696e32
Julia Bonserio <juliabonserio@live.com.ar>
2020-11-01 16:53:54 -0300

    julia--vista-foro

--- Frontend PS/js/pages/foro.js
+++ Frontend PS/js/pages/foro.js
@@ -1,39 +1,68 @@
 $(document).ready(function () {
 
-fetch('http://example.com/movies.json')
-    .then(response => response.json())
+
+//Configuracion de carga de comentarios
+
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:5001/api/EstudianteCurso/cursos/2", //ruta de ejemplo
+        dataType: "json",
+        success: function(data) {
+
+            $("tr:has(td)").remove();
+            $.each(data, function(i, item) {
+
+                // var text = document.createElement("p");
+                // text.innerHTML = item.estado;
+                // document.getElementById("forum-get-coment").appendChild(text);
+                // document.getElementById("forum-get-coment").appendChild(text);
+                
+                var tr = document.createElement('tr');
+                var td = document.createElement('td');
+
+                var td1 = document.createElement('td');
+                td1.textContent = item.estado;
+                tr.append(td);
+                tr.append(td1);
+                $('#tb_comentarios').append(tr);
+            });
+        },
+        error: function(error) {
+            console.log(error.message);
+            alert('error');
+        }
+    });
 
 
 
 
-
-
-
-
-
-
-//Configuracion de envio de comentarios
+//Seteo de envio de comentarios
     
-    var seccionForo = $('.centro');
+    var seccionForo = $('#forum-send-coment');
 
-    var cursoComentar = `<div id="seccionListado" style="margin: 2rem">
-                        <label>Envíe un comentario</label>
-                        <input id="inputComentario" type="text"/>
-                        <button id="Enviar">Enviar</button>                        
-                    </div>
-                    <br>`;
+    var claseComentar = `<textarea id="inputComentario" maxlength="200" placeholder="Solo se permiten 200 caracteres "></textarea> 
+                        <button id="Enviar" style="float: right;">Enviar</button>`;
                     
-    seccionForo.append(cursoComentar);
-    
+    seccionForo.append(claseComentar);
+
+    var comentario = $("#inputComentario").val();
+    var foroId = 1;
+
     $("#Enviar").on("click",function (){    
-        
-        
-
-
-
-
+        $.ajax({
+            url: 'https://localhost:44xxx/api/Comentario?'+foroId,
+            data: JSON.stringify(comentario),
+            type: "POST",
+            dataType: 'JSON',
+            contentType: "application/json",
+    
+    
+            success: function() {
+                location.reload();
+            }
+        });
 
     });
 
 
-})
+});
