b26516c45bdc3795329e943f9b9312800f000c10
octavio <octaviojorge37@gmail.com>
2020-11-02 01:15:57 -0300

    inscripcion

--- Frontend PS/js/pages/inscripciones.js
+++ Frontend PS/js/pages/inscripciones.js
@@ -50,3 +50,65 @@ $(document).on('click', '.detalles', function()
 
     contenido.append(text);
 });
+
+let objeto = {
+    "cursoID": 1, //valor preseteado
+    "estudianteID": 1,
+    "estado": "aprobado"
+}
+function PostInscripcion() {
+        fetch(`https://localhost:44326/api/EstudianteCurso`, {
+                method: 'POST',
+                mode: 'cors',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            })
+            .then(response => {
+                return response.json()
+            })
+            .then(function(objeto) {
+                alert('Incripcion realizada');
+                
+            })
+            .catch(err => console.log('ERROR: ' + err));
+
+  
+}
+
+// myform.submit((event) => {
+//     event.preventDefault();
+
+//     // Change to your service ID, or keep using the default service
+//     var service_id = "default_service";
+//     var template_id = "template_3SA9LsqQ";
+
+//     const cargandoGif = document.querySelector('#cargando');
+//     cargandoGif.style.display = 'block';
+
+//     const enviado = document.createElement('img');
+//     enviado.src = 'img/mail.gif';
+//     enviado.style.display = 'block';
+//     enviado.width = '150';
+
+//     emailjs.sendForm(service_id, template_id, myform[0])
+//         .then(() => {
+//             cargandoGif.style.display = 'none';
+//             document.querySelector('#loaders').appendChild(enviado);
+
+//             setTimeout(() => {
+//                 compra.vaciarLocalStorage();
+//                 enviado.remove();
+//                 window.location = "index.html";
+//             }, 2000);
+
+
+//         }, (err) => {
+//             alert("Error al enviar el email\r\n Response:\n " + JSON.stringify(err));
+//             // myform.find("button").text("Send");
+//         });
+
+//     return false;
+
+// });
+
