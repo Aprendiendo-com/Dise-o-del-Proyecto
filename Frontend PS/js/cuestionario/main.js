95923dcc04d8a2add906230601b2db06537f748e
Danieltq04 <daniel.tq04@gmail.com>
2020-11-23 15:50:57 -0300

    commit de resolucion de cuestionario

--- Frontend PS/js/cuestionario/main.js
+++ Frontend PS/js/cuestionario/main.js
@@ -2,7 +2,8 @@ import {
     CuestionarioTodoDTO,
     CuestionarioACorregirDTO,
     RespuestaAlumnoDTO,
-    Registro
+    Registro,
+    PreguntaConRespuestaAlumnoDTO
 } from "./Constant.js";
 
 import * as CuestionarioService from "./CuestionarioService.js";
@@ -12,15 +13,15 @@ window.onload = () => {
     CargarCuestionario();
     $('#enviar-cuestionario').on("click", EnviarCuestionario); //CUANDO RECIBE POR PARÁMETRO ENTRA DIRECTO
 }
+const ALUMNOID = 1;
 
 function CargarCuestionario() {
-    if(localStorage.getItem('claseU')){
+    if (localStorage.getItem('claseU')) {
         var idClase = JSON.parse(localStorage.getItem('claseU')).claseId;
-    }
-    else{
+    } else {
         var idClase = 1;
     }
-    CuestionarioService.default( idClase ).then(x => LoadCuestionario(x));
+    CuestionarioService.default(idClase).then(x => LoadCuestionario(x));
 }
 
 function LoadCuestionario(CuestionarioTodoDTO) {
@@ -31,19 +32,19 @@ function LoadCuestionario(CuestionarioTodoDTO) {
 
     var i = 1;
     var lista = [];
-    
-    var valorPregunta = Math.round(10/CuestionarioTodoDTO.preguntas.length * 100) / 100;
-    CuestionarioTodoDTO.preguntas.forEach(preguntas => {
 
-       informacion.innerHTML += `<div class="filas">
-       <div class="info" id=${"info"+i}>${"Pregunta por: "+ valorPregunta + "pts."}</div>
+    var lista_preguntas = [];
+    CuestionarioTodoDTO.preguntas.forEach(preguntas => {
+        lista_preguntas.push(preguntas);
+        informacion.innerHTML += `<div class="filas">
+       <div class="info" id=${"info"+i} value=${preguntas.calificacionParcial}>${"Pregunta por: "+ preguntas.calificacionParcial + "pts."}</div>
        <div class="pregyresp-grid" id=${i}>
-       <div class="pregunta" id="preguntaId">
+       <div class="pregunta" id=${"preguntaId"+i}>
        <label for="preg" id="preguntaLabelInicio"> ${("Pregunta "+ i + "- ")} </label>
        <label for="preg" id="preguntaLabelDescripcion"> ${(preguntas.descripcion)} </label>
        </div>`;
 
-       var j = 1;
+        var j = 1;
         var lista_respuestas = [];
         preguntas.respuestas.forEach(respuestas => {
             lista_respuestas.push(j);
@@ -63,16 +64,15 @@ function LoadCuestionario(CuestionarioTodoDTO) {
     })
 
     localStorage.setItem('ListaDimensiones', JSON.stringify(lista));
-
+    sessionStorage.setItem('preguntas', JSON.stringify(lista_preguntas));
     //SETEAR DESDE EL LOCALSTORAGE
-    var alumno = 1;
-    RegistroService.default().then(x => RevisarRegistro(x, CuestionarioTodoDTO.claseId, alumno));
+    RegistroService.default().then(x => RevisarRegistro(x, CuestionarioTodoDTO.claseId, ALUMNOID));
     //LO ACABO DE COMENTAR
 }
 
 function RevisarRegistro(registro, claseId, estudianteId) {
     //TAMBIÉN DESHABILITAR SI NO HAY UN ESTUDIANTE
-    console.log(sessionStorage.getItem("UsuarioIdToken"));
+
     registro.forEach(reg => {
         if (reg.claseId == claseId && reg.estudianteId == estudianteId) {
             DeshabilitarOpciones();
@@ -86,19 +86,27 @@ function RevisarRegistro(registro, claseId, estudianteId) {
 
 function EnviarCuestionario() {
 
-    debugger
-    var lista = JSON.parse(localStorage.getItem("ListaDimensiones"));
     var cuestionario_respuestas = new CuestionarioACorregirDTO;
-    cuestionario_respuestas.respuestas = [];
-    for (var i = 1, length = lista.length; i < length + 1; i++) {
-        //cuestionario_respuestas.cuestionarioId = 11; //CAMBIAR ESTO
-        cuestionario_respuestas.claseId = sessionStorage.getItem("idClase"); //CAMBIAR ESTO
-        var respuesta_descripcion = new RespuestaAlumnoDTO($(`input[name=${"respuesta"+i}]:checked`).val())
-        cuestionario_respuestas.respuestas.push(respuesta_descripcion);
-    }
+    cuestionario_respuestas.preguntas = [];
+    cuestionario_respuestas.claseId = sessionStorage.getItem("idClase"); //CAMBIAR ESTO
+    var lista_preguntas = [];
+    var preguntas = JSON.parse(sessionStorage.getItem('preguntas'));
+    var i = 1;
+    preguntas.forEach(pregunta => {
+        var respuesta_seleccion = new RespuestaAlumnoDTO($(`input[name=${"respuesta"+i}]:checked`).val())
+        var pregunta_con_respuesta = new PreguntaConRespuestaAlumnoDTO(
+            pregunta.descripcion, pregunta.calificacionParcial, respuesta_seleccion);
+        lista_preguntas.push(pregunta_con_respuesta);
+        i++;
+    })
+
+    cuestionario_respuestas.preguntas = lista_preguntas;
+
+    /*console.log(JSON.stringify(cuestionario_respuestas));*/
+
 
     //MANDAR EL CUESTIONARIO A LA API
-    //console.log(JSON.stringify(cuestionario_respuestas));
+
     var options = {
         method: 'POST',
         headers: {
@@ -120,24 +128,22 @@ function EnviarCuestionario() {
             return json;
         })
         .catch(err => console.log('ERROR: ' + err))
+
 }
 
 function agregarCalificacion(resolucion) {
-    console.log(resolucion);
     var informacion = document.getElementById("header-cuestionario");
     informacion.innerHTML += `<div class="calificacion"> ${("--> CALIFICACION: " + resolucion.calificacionTotal +"/10 <--")} </div>`;
-    
 
     //MODIFICADO
     var i = 1;
     resolucion.respuestas.forEach(respuesta => {
-        if(respuesta.respuestaAlumno == respuesta.respuestaCorrecta){
-            var informacion = document.getElementById("info"+i);
+        if (respuesta.respuestaAlumno == respuesta.respuestaCorrecta) {
+            var informacion = document.getElementById("info" + i);
             informacion.innerHTML += `</br><div class="info-estado-correcto"> Estado: Correcta </div>`;
             console.log("CORRECTA");
-        }
-        else{
-            var informacion = document.getElementById("info"+i);
+        } else {
+            var informacion = document.getElementById("info" + i);
             informacion.innerHTML += `</br><div class="info-estado-incorrecto"> Estado: Incorrecta </div>`;
             console.log("FALSA");
         }
@@ -147,10 +153,9 @@ function agregarCalificacion(resolucion) {
     RegistrarCalificacion(resolucion);
 }
 
-function RegistrarCalificacion(resolucion){
+function RegistrarCalificacion(resolucion) {
     var idClase = sessionStorage.getItem("idClase");
-    var alumno = 1;
-    var registro = new Registro(alumno,idClase,resolucion.calificacionTotal);
+    var registro = new Registro(ALUMNOID, idClase, resolucion.calificacionTotal);
 
     var options = {
         method: 'POST',
@@ -167,13 +172,12 @@ function RegistrarCalificacion(resolucion){
             return response.json()
         })
         .then(json => {
-            
             return json;
         })
         .catch(err => console.log('ERROR: ' + err))
 }
 
-function DeshabilitarBoton(){
+function DeshabilitarBoton() {
     document.getElementById("enviar-cuestionario").disabled = true;
 }
 
@@ -189,8 +193,5 @@ function DeshabilitarOpciones() {
         })
         i++;
     })
-    
+
 }
-
-
-
